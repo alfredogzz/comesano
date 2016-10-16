f08961d68608ed0bf579735913f99b86bc1325e8
gtzefrain <gtz.efrain@gmail.com>
2016-10-16 14:16:31 -0500

    Angular Infinite Digest Bug FIx

--- static/angularapp/js/controllers/homeCtrl.js
+++ static/angularapp/js/controllers/homeCtrl.js
@@ -2,7 +2,7 @@
   var ReporteCtrl;
 
   ReporteCtrl = function($scope, $rootScope, $state, $stateParams, $http, $uibModal, toaster) {
-    $scope.test = 'esto es un test'
+    $scope.test = 'esto es un test';
   }
 
   angular.module('vegApp').controller('homeCtrl', ReporteCtrl);
